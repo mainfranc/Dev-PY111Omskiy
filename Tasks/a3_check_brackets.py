e7b6a5db2f20fc1d52c80c402c7aaa4ed2589b7a
mainfranc <igoromskiy@gmail.com>
2020-12-19 13:27:01 +0300

    tested

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,16 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
-    return False
+    num_of_opened = 0
+    num_of_closed = 0
+    result = True
+    for i in brackets_row:
+        if i == '(':
+            num_of_opened += 1
+        else:
+            num_of_closed += 1
+        if num_of_closed > num_of_opened: result = False
+    if result:
+        if num_of_closed != num_of_opened:
+            result = False
+    return result
