88011d6e115c4fc609eaa79191888578c245d9be
mainfranc <igoromskiy@gmail.com>
2020-12-25 16:58:25 +0300

    change implemented

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,18 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
+    if arr:
+        len_arr = len(arr)
+        len_of_curr_diap = len(arr) // 2
+        beg_diap = 0
+        while elem != arr[beg_diap + len_of_curr_diap - 1] and len_of_curr_diap != 1:
+            if elem > arr[beg_diap + len_of_curr_diap - 1]: beg_diap += len_of_curr_diap
+            if not len_of_curr_diap % 2:
+                len_of_curr_diap //= 2
+            else:
+                len_of_curr_diap = len_of_curr_diap // 2 + 1
+        if arr[beg_diap] == elem:
+            return beg_diap
+        if arr[beg_diap + len_of_curr_diap - 1] == elem:
+            return beg_diap + len_of_curr_diap - 1
     return None
