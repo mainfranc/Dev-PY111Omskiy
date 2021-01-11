401d5d216a57b3f24310ab65c0aff0357a4d6f3a
mainfranc <igoromskiy@gmail.com>
2021-01-11 18:17:46 +0300

    changed according the comments

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+import queue
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +11,16 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    v_queue = queue.Queue()
+    v_queue.put(start_node)
+    visited_nodes = [start_node]
+
+    while True:
+        legacies = v_queue.get()
+
+        for node in g[legacies]:
+            if not (node in visited_nodes):
+                visited_nodes.append(node)
+                v_queue.put(node)
+        if not v_queue.queue: return visited_nodes
+
