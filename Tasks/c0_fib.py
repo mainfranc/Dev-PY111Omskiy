4a9d979022b67473343d327fe4b836c713069d1c
mainfranc <74810953+mainfranc@users.noreply.github.com>
2021-01-16 12:59:15 +0300

    Update c0_fib.py

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -7,7 +7,6 @@ def fib_recursive(n: int) -> int:
     """
     cache = {}
     result = fib_rec_calc(n, cache)
-    cache = {}
     return result
 
 def fib_rec_calc(n: int, cache) -> int:
@@ -15,7 +14,7 @@ def fib_rec_calc(n: int, cache) -> int:
         raise ValueError()
     if n == 1 or n == 2:
         return 1
-    if cache.get(n, 0):
+    if cache.get(n):
         return cache[n]
     result =  fib_rec_calc(n-1, cache) + fib_rec_calc(n-2, cache)
     cache[n] = result
@@ -34,11 +33,11 @@ def fib_iterative(n: int) -> int:
     if n < 1:
         raise ValueError()
 
-    results = [0, 1]
+    r1, r2 = 0, 1
     for i in range(0, n - 1):
-        last_member = results[-1] + results[-2]
-        results[-2] = results[-1]
-        results[-1] = last_member
-    return results[len(results) - 1]
+        last_member = r1 + r2
+        r1 = r2
+        r2 = last_member
+    return r2
 
 
