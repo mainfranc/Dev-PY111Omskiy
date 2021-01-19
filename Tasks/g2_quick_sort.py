e6ca20a6ca7acc10dd0b83d45300a256711171b7
mainfranc <igoromskiy@gmail.com>
2021-01-19 19:11:34 +0300

    added

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,4 +1,5 @@
 from typing import List
+from statistics import median
 
 
 def sort(container: List[int]) -> List[int]:
@@ -8,4 +9,19 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    lst_less = []
+    lst_equal = []
+    lst_greater = []
+
+    if len(container) > 1:
+        base_elem = median(container)
+        for elem in container:
+            if elem < base_elem:
+                lst_less.append(elem)
+            elif elem == base_elem:
+                lst_equal.append(elem)
+            elif elem > base_elem:
+                lst_greater.append(elem)
+        return sort(lst_less) + lst_equal + sort(lst_greater)
+    else:
+        return container
